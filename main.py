468ca92310496899fb9ee516037725c4ae282ed6
DennisseAguirre <dennisseaguirre3@gmail.com>
2023-08-20 01:57:01 -0500

    Rellenando formulario vacío

--- main.py
+++ main.py
@@ -144,112 +144,6 @@ def llenar_diccionario(dic_lleno,dic_vacio):
 diccionario = llenar_diccionario(dic_lleno,dic_vacio)
 print("diccionario llenado:",diccionario)
 
-# # Escribir texto
-# cv2.putText(img_vacio, texto, ubicacion, font, tamañoLetra, colorLetra, grosorLetra)
-
-
-# def escribirImagen(img, diccionario):
-#     color = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#
-#     cv2.imshow("gray", gray)
-#     cv2.waitKey(0)
-#
-#     # Calcular la mediana del color de fondo para determinar si es color o blanco
-#     median_color = np.median(color)
-#     background_is_white = median_color > 245  # Ajusta este valor según tus necesidades
-#
-#     # Si el fondo es de color, utilizar la imagen umbralizada, de lo contrario usar la escala de grises
-#     if background_is_white:
-#         binary = cv2.adaptiveThreshold(
-#             gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
-#         )
-#     else:
-#         thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-#         binary = cv2.adaptiveThreshold(
-#             thresholded, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
-#         )
-#
-#     cv2.imshow("binary", binary)
-#     cv2.waitKey(0)
-#
-#     cv2.imshow("Imagen llenada", img)
-#     cv2.waitKey(0)
-#
-#     # Buscar contornos en la imagen binarizada
-#     contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-#     # Lista para almacenar las regiones de los textbox
-#     textbox_regions = []
-#
-#     for contour in contours:
-#         # Filtrar contornos por área y forma aproximada a un rectángulo
-#         area = cv2.contourArea(contour)
-#         x, y, w, h = cv2.boundingRect(contour)
-#
-#         # Ajustar valores según el tamaño de tus campos
-#         if 500 < area:
-#             textbox_regions.append((x, y, x + w, y + h))
-#
-#     # Procesar el contenido de cada región
-#     textbox_contents = {}
-#
-#     for region in textbox_regions:
-#         x1, y1, x2, y2 = region
-#         roi = gray[y1:y2, x1:x2]
-#         content = pytesseract.image_to_string(roi, lang="spa")
-#         textbox_contents[(x1, y1, x2, y2)] = content
-#
-#     # Dibujar las coordenadas en la imagen
-#     for coords in textbox_contents.keys():
-#         x1, y1, x2, y2 = coords
-#         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-#
-#     # Procesar y almacenar el contenido a la izquierda de cada región de textbox en un diccionario
-#     # contents_dictionary = {}
-#
-#     for coords, content in textbox_contents.items():
-#         x1, y1, x2, y2 = coords
-#         roi_left = gray[y1:y2, 0:x1]
-#         content_left = pytesseract.image_to_string(roi_left, lang="spa")
-#         # Eliminar espacios en blanco alrededor de la clave y valor
-#         #
-#         # texto_sin_tildes = unidecode(content_left)
-#         # texto_sin_tilde = unidecode(content)
-#         # # Quitar espacios
-#         # content_left = texto_sin_tildes.strip()
-#         # content = texto_sin_tilde.strip()
-#         # # Quitar el punto al final del texto, si existe
-#         # if content_left.endswith("."):
-#         #     content_left = content_left[:-1]
-#         # elif content.endswith("."):
-#         #     content = content[:-1]
-#         #
-#         # contents_dictionary[content_left] = content
-#     # Características del texto
-#     for region in textbox_regions:
-#         x1, y1, x2, y2 = region
-#
-#     texto = "Quito"
-#     ubicacion = (10, 25)
-#     font = cv2.FONT_HERSHEY_TRIPLEX
-#     tamañoLetra = 10
-#     colorLetra = (0,0,0)
-#     grosorLetra = 10
-#     for dic in diccionario.keys():
-#         if content_left == dic:
-#             cv2.putText(img, texto, ubicacion, font, tamañoLetra, colorLetra, grosorLetra)
-#
-#
-#     # Mostrar la imagen
-#     cv2.imshow("Imagen llenada", img)
-#     cv2.waitKey(0)
-#     # cv2.destroyAllWindows()
-#
-#
-# lista = escribirImagen(img_vacio,diccionario)
-# print("lista:",lista)
-
 
 def llenar_campos_en_imagen(img, dic_lleno, dic_vacio):
 
@@ -318,49 +212,24 @@ def llenar_campos_en_imagen(img, dic_lleno, dic_vacio):
         textbox_contents[(x1, y1, x2, y2)] = content
 
     # Dibujar las coordenadas en la imagen
-    for coords in textbox_contents.keys():
+    for coords, valor_lleno in zip(textbox_contents.keys(), dic_lleno.values()):
         x1, y1, x2, y2 = coords
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
         print("x1, y1, x2, y2", x1, y1, x2, y2)
 
-    # # Procesar la imagen y obtener las coordenadas de los textbox
-    # textbox_regions = []
-    # contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # for contour in contours:
-    #     area = cv2.contourArea(contour)
-    #     if 500 < area:
-    #         x, y, w, h = cv2.boundingRect(contour)
-    #         textbox_regions.append((x, y, x + w, y + h))
-    # print("x, y, x + w, y + h:", x, y, x + w, y + h)
+        # Agregar el valor desde dic_lleno en la imagen utilizando putText
+        cv2.putText(
+            img,
+            valor_lleno,
+            (x1 + 5, y1 + 20),  # Ajusta la posición según tus necesidades
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.6,
+            (0, 0, 0),
+            1,
+            cv2.LINE_AA,
+        )
 
-    # Crear una copia de la imagen para escribir en ella
     img_con_texto = img.copy()
-
-    # Recorrer las claves del diccionario vacío
-    for clave_vacio in dic_vacio:
-        # Buscar la clave en el contenido de la imagen
-        if clave_vacio in img_content:
-            # Si la clave está en el contenido, obtener el valor correspondiente del diccionario lleno
-            valor_lleno = dic_lleno.get(clave_vacio, '')
-
-            # Encontrar la posición de la clave en la imagen
-            posicion = img_content.find(clave_vacio)
-            print("posicion:",x1,y1)
-            # Agregar el valor en la imagen utilizando putText
-            for coords in textbox_contents.keys():
-                x1, y1, x2, y2 = coords
-                # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(
-                    img_con_texto,
-                    valor_lleno,
-                    (x1+5, y1+20),  # Ajusta la posición según tus necesidades
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6,
-                    (0, 0, 0),
-                    1,
-                    cv2.LINE_AA,
-                )
-
 
     return img_con_texto
 
@@ -374,7 +243,3 @@ img_con_campos_llenos = llenar_campos_en_imagen(img_a_llenar, diccionario, dic_v
 # Mostrar la imagen con los campos llenados y escritos
 cv2.imshow("Imagen con campos llenados", img_con_campos_llenos)
 cv2.waitKey(0)
-
-
-
-
